07bb859a3326cdef17a05338a83548ea322e36a3
Muhammad Zafar <xafarr@gmail.com>
2022-07-08 16:05:08 +1000

    Set to show python version with virtualenv

--- powerlevel10k/p10k.zsh
+++ powerlevel10k/p10k.zsh
@@ -864,7 +864,7 @@
   # Python virtual environment color.
   typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=37
   # Don't show Python version next to the virtual environment name.
-  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=true
+  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
   # If set to "false", won't show virtualenv if pyenv is already shown.
   # If set to "if-different", won't show virtualenv if it's the same as pyenv.
   typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_WITH_PYENV=false
