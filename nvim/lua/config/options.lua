29353fb20ced3d41acfbaf39d4dd6d5103b3e641
Muhammad Zafar <xafarr@gmail.com>
2022-08-15 23:24:41 +1000

    Add general options through autocommand

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,6 +6,16 @@ vim.cmd [[
   colorscheme nordfox
 ]]
 
+vim.cmd [[
+  augroup _general_settings
+    autocmd!
+    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
+    autocmd BufWinEnter * :set formatoptions-=cro
+    autocmd FileType qf set nobuflisted
+  augroup end
+]]
+
 -- General Options
 set.backup = false                                   -- creates a backup file
 set.clipboard = { "unnamed", "unnamedplus" }         -- allows neovim to access the system clipboard
