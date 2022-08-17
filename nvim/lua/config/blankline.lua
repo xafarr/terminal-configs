ad24da14afca9ee8f5a1c70deb46079dfba14376
Muhammad Zafar <xafarr@gmail.com>
2022-08-17 20:01:29 +1000

    Show line ending character

--- nvim/lua/config/blankline.lua
+++ nvim/lua/config/blankline.lua
@@ -3,6 +3,5 @@ local blankline = require("indent_blankline")
 blankline.setup {
   show_current_context = true,
   show_current_context_start = true,
-  use_treesitter = true,
-  use_treesitter_scope = true,
+  show_end_of_line = true,
 }
