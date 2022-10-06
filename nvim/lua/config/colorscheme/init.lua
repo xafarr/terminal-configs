dcb014c1c94516196cbad7dd79769e4c79d1b4b3
Muhammad Zafar <xafarr@gmail.com>
2022-10-07 10:17:37 +1100

    Move colorscheme configs in a directory to easy management

---
+++ nvim/lua/config/colorscheme/init.lua
@@ -0,0 +1,6 @@
+vim.g.edge_style = "aura"
+vim.g.edge_better_performance = 1
+vim.g.edge_colors_override = { bg3 = { "#cbe5f8", "237" }, bg2 = { "#e3e4e5", "237" } }
+
+-- Colorscheme
+vim.cmd("colorscheme edge")
