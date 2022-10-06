fa141fed43e8e4321cdddf3db30922bb0f8ac1f7
Muhammad Zafar <xafarr@gmail.com>
2022-10-07 02:26:18 +1100

    Change Visual selection color for edge theme

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,26 +1,29 @@
-local nightfox = require("nightfox")
+--[[ local nightfox = require("nightfox") ]]
 
-nightfox.setup({
-    dim_inactive = true,
-    options = {
-        styles = {
-            comments = "italic",
-            keywords = "bold",
-            functions = "italic",
-        },
-        inverse = {
-            search = true,
-            match_paren = true,
-        },
-        modules = {
-            cmp = true,
-            gitsigns = true,
-            nvimtree = true,
-            treesitter = true,
-            telescope = true,
-        },
-    },
-})
+--[[ nightfox.setup({ ]]
+--[[     dim_inactive = true, ]]
+--[[     options = { ]]
+--[[         styles = { ]]
+--[[             comments = "italic", ]]
+--[[             keywords = "bold", ]]
+--[[             functions = "italic", ]]
+--[[         }, ]]
+--[[         inverse = { ]]
+--[[             search = true, ]]
+--[[             match_paren = true, ]]
+--[[         }, ]]
+--[[         modules = { ]]
+--[[             cmp = true, ]]
+--[[             gitsigns = true, ]]
+--[[             nvimtree = true, ]]
+--[[             treesitter = true, ]]
+--[[             telescope = true, ]]
+--[[         }, ]]
+--[[     }, ]]
+--[[ }) ]]
+
+vim.g.edge_style = "aura"
+vim.g.edge_colors_override = { bg3 = { "#A6D3F3", "237" } }
 
 -- Colorscheme
 vim.cmd("colorscheme edge")
