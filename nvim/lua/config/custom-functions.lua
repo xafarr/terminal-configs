3f16994614c38b62a0930f3e0f7bd9c463e500f2
Muhammad Zafar <xafarr@gmail.com>
2022-11-16 12:19:44 +1100

    Add dockerls as a formatter for Dockerfile as null-ls doesn't have one

--- nvim/lua/config/custom-functions.lua
+++ nvim/lua/config/custom-functions.lua
@@ -3,10 +3,9 @@ M.format = function(bufnr)
     vim.lsp.buf.format({
         bufnr = bufnr,
         filter = function(client)
-            return client.name == "null-ls"
+            return client.name == "null-ls" or client.name == "dockerls"
         end,
         async = true,
     })
 end
 return M
-
