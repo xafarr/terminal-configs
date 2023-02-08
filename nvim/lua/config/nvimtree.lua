f857ec1d6af5325f17dff32e4018672190db9357
Muhammad Zafar <xafarr@gmail.com>
2023-02-08 08:05:03 +0500

    Open nvim tree (explorer) when opened file is directory

--- nvim/lua/config/nvimtree.lua
+++ nvim/lua/config/nvimtree.lua
@@ -7,6 +7,28 @@ local nvim_tree_config = require("nvim-tree.config")
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local function open_nvim_tree(data)
+    local directory = vim.fn.isdirectory(data.file) == 1
+    if not directory then
+        return
+    end
+
+    -- create a new, empty buffer
+    vim.cmd.enew()
+
+    -- wipe the directory buffer
+    vim.cmd.bw(data.buf)
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+end
+
+-- NvimTree start up
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 nvim_tree.setup({
     remove_keymaps = { "<Tab>" },
     update_focused_file = {
