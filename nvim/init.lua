ed6877ccf0b4f29630e02643d88273f4b78d829e
Muhammad Zafar <xafarr@gmail.com>
2023-03-06 23:31:28 +1100

    Rename autocommands to autocmds to match the nvim actual functions and also added couple of new autocmds

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 require("config.plugins")
 require("config.options")
-require("config.autocommands")
+require("config.autocmds")
 require("config.keymaps")
 require("config.colorscheme")
 require("config.orgmode")
---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,105 @@
+local api = vim.api
+
+-- Highlight on yank
+local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    pattern = "*",
+    group = yankGrp,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
+-- Auto toggle hlsearch
+local ns = vim.api.nvim_create_namespace("toggle_hlsearch")
+local function toggle_hlsearch(char)
+    if vim.fn.mode() == "n" then
+        local keys = { "<CR>", "n", "N", "*", "#", "?", "/" }
+        local new_hlsearch = vim.tbl_contains(keys, vim.fn.keytrans(char))
+
+        if vim.opt.hlsearch:get() ~= new_hlsearch then
+            vim.opt.hlsearch = new_hlsearch
+        end
+    end
+end
+vim.on_key(toggle_hlsearch, ns)
+
+-- windows to close with "q"
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "OverseerForm",
+        "OverseerList",
+        "checkhealth",
+        "floggraph",
+        "fugitive",
+        "git",
+        "help",
+        "lspinfo",
+        "man",
+        "neotest-output",
+        "neotest-summary",
+        "qf",
+        "query",
+        "spectre_panel",
+        "startuptime",
+        "toggleterm",
+        "tsplayground",
+        "vim",
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    end,
+})
+api.nvim_create_autocmd("FileType", { pattern = "cheat", command = [[nnoremap <buffer><silent> q :bdelete!<CR>]] })
+
+-- hidden buffer
+api.nvim_create_autocmd("FileType", {
+    pattern = { "qf", "TelescopePrompt" },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+    end,
+})
+
+-- show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, { pattern = "*", command = "set cursorline", group = cursorGrp })
+api.nvim_create_autocmd(
+    { "InsertEnter", "WinLeave" },
+    { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+-- don't auto comment new line
+api.nvim_create_autocmd("BufWinEnter", { pattern = "*", command = [[set formatoptions-=cro]] })
+
+-- create directories when needed, when saving a file
+api.nvim_create_autocmd("BufWritePre", {
+    group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+    command = [[call mkdir(expand('<afile>:p:h'), 'p')]],
+})
+
+-- mark the last edited position across buffers
+api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    command = 'execute "normal! mI"',
+})
+
+-- Check if we need to reload the file when it changed
+api.nvim_create_autocmd("FocusGained", { command = "checktime" })
+
+-- vim resize event (don't know what it does)
+api.nvim_create_autocmd("VimResized", {
+    pattern = "*",
+    command = "tabdo wincmd =",
+})
