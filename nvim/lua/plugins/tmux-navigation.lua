84aa9f9360eb2fd1cff4d21f89d8b14787e48ba0
Muhammad Zafar <xafarr@gmail.com>
2023-03-09 21:44:25 +1100

    Change tmux and vim navigation keys to use Ctrl instead of Alt/Option with arrow keys

--- nvim/lua/plugins/tmux-navigation.lua
+++ nvim/lua/plugins/tmux-navigation.lua
@@ -6,12 +6,12 @@ return {
       local tmux_navigation = require("nvim-tmux-navigation")
       tmux_navigation.setup({
         keybindings = {
-          left = "<M-Left>",
-          down = "<M-Down>",
-          up = "<M-Up>",
-          right = "<M-Right>",
-          last_active = "<C-def>",
-          next = "<C-abc>",
+          left = "<C-Left>",
+          down = "<C-Down>",
+          up = "<C-Up>",
+          right = "<C-Right>",
+          --last_active = "<C-def>",
+          --next = "<C-abc>",
         },
       })
     end
