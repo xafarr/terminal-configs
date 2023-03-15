fb1e692e209a0d18d05bd4eb9678a40766758b49
Muhammad Zafar <xafarr@gmail.com>
2023-03-15 17:11:29 +1100

    Remove unnecessary plugin

--- nvim/lua/plugins/lsp/dap.lua
+++ nvim/lua/plugins/lsp/dap.lua
@@ -5,7 +5,6 @@ return {
             { "rcarriga/nvim-dap-ui" },
             { "theHamsta/nvim-dap-virtual-text" },
             { "nvim-telescope/telescope-dap.nvim" },
-            { "jbyuki/one-small-step-for-vimkind" },
             { "jay-babu/mason-nvim-dap.nvim" },
         },
         -- stylua: ignore
