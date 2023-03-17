ede990a7db932f86d541bd961e86c441346a95e1
Muhammad Zafar <xafarr@gmail.com>
2023-03-17 18:16:42 +1100

    Change dap config and add go dap adaptor

--- nvim/lua/plugins/lsp/dap.lua
+++ nvim/lua/plugins/lsp/dap.lua
@@ -1,6 +1,7 @@
 return {
     {
         "mfussenegger/nvim-dap",
+        event = "BufRead",
         dependencies = {
             { "rcarriga/nvim-dap-ui" },
             { "theHamsta/nvim-dap-virtual-text" },
@@ -9,26 +10,45 @@ return {
         },
         -- stylua: ignore
         keys = {
-            { "<leader>dR", function() require("dap").run_to_cursor() end, desc = "Run to Cursor", },
-            { "<leader>dE", function() require("dapui").eval(vim.fn.input "[Expression] > ") end, desc = "Evaluate Input", },
-            { "<leader>dC", function() require("dap").set_breakpoint(vim.fn.input "[Condition] > ") end, desc = "Conditional Breakpoint", },
-            { "<leader>dU", function() require("dapui").toggle() end, desc = "Toggle UI", },
-            { "<leader>db", function() require("dap").step_back() end, desc = "Step Back", },
-            { "<F9>", function() require("dap").continue() end, desc = "Continue", },
-            { "<leader>dd", function() require("dap").disconnect() end, desc = "Disconnect", },
-            { "<leader>de", function() require("dapui").eval() end, mode = {"n", "v"}, desc = "Evaluate", },
-            { "<leader>dg", function() require("dap").session() end, desc = "Get Session", },
-            { "<leader>dh", function() require("dap.ui.widgets").hover() end, desc = "Hover Variables", },
-            { "<leader>dS", function() require("dap.ui.widgets").scopes() end, desc = "Scopes", },
-            { "<F7>", function() require("dap").step_into() end, desc = "Step Into", },
-            { "<F8>", function() require("dap").step_over() end, desc = "Step Over", },
-            { "<leader>dp", function() require("dap").pause.toggle() end, desc = "Pause", },
-            { "<leader>dq", function() require("dap").close() end, desc = "Quit", },
-            { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL", },
-            { "<leader>ds", function() require("dap").continue() end, desc = "Start", },
-            { "<leader>dt", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint", },
-            { "<leader>dx", function() require("dap").terminate() end, desc = "Terminate", },
-            { "<leader>du", function() require("dap").step_out() end, desc = "Step Out", },
+            { "<leader>dR", function() require("dap").run_to_cursor() end,                               desc =
+            "Run to Cursor", },
+            { "<leader>dE", function() require("dapui").eval(vim.fn.input "[Expression] > ") end,
+                                                                                                             desc =
+                "Evaluate Input", },
+            { "<leader>dC", function() require("dap").set_breakpoint(vim.fn.input "[Condition] > ") end,
+                                                                                                             desc =
+                "Conditional Breakpoint", },
+            { "<leader>dU", function() require("dapui").toggle() end,                                    desc =
+            "Toggle UI", },
+            { "<leader>db", function() require("dap").step_back() end,                                   desc =
+            "Step Back", },
+            { "<F9>",       function() require("dap").continue() end,                                    desc =
+            "Continue", },
+            { "<leader>dd", function() require("dap").disconnect() end,                                  desc =
+            "Disconnect", },
+            { "<leader>de", function() require("dapui").eval() end,                                      mode = { "n",
+                "v" },                                                                                                                    desc =
+            "Evaluate", },
+            { "<leader>dg", function() require("dap").session() end,                                     desc =
+            "Get Session", },
+            { "<leader>dh", function() require("dap.ui.widgets").hover() end,                            desc =
+            "Hover Variables", },
+            { "<leader>dS", function() require("dap.ui.widgets").scopes() end,                           desc = "Scopes", },
+            { "<F7>",       function() require("dap").step_into() end,                                   desc =
+            "Step Into", },
+            { "<F8>",       function() require("dap").step_over() end,                                   desc =
+            "Step Over", },
+            { "<leader>dp", function() require("dap").pause.toggle() end,                                desc = "Pause", },
+            { "<leader>dq", function() require("dap").close() end,                                       desc = "Quit", },
+            { "<leader>dr", function() require("dap").repl.toggle() end,                                 desc =
+            "Toggle REPL", },
+            { "<leader>ds", function() require("dap").continue() end,                                    desc = "Start", },
+            { "<leader>dt", function() require("dap").toggle_breakpoint() end,                           desc =
+            "Toggle Breakpoint", },
+            { "<leader>dx", function() require("dap").terminate() end,                                   desc =
+            "Terminate", },
+            { "<leader>du", function() require("dap").step_out() end,                                    desc =
+            "Step Out", },
         },
         config = function(plugin, opts)
             require("nvim-dap-virtual-text").setup({
@@ -60,10 +80,9 @@ return {
     },
     {
         "jay-babu/mason-nvim-dap.nvim",
-        event = "BufRead",
         dependencies = {
             "williamboman/mason.nvim",
-            "mfussenegger/nvim-dap",
+            "leoluz/nvim-dap-go",
         },
         cmd = { "DapInstall", "DapUninstall" },
         config = function()
