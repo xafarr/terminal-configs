135fd9201c4aff30b5f3ce04823346ddb9138475
Muhammad Zafar <xafarr@gmail.com>
2023-03-20 23:10:54 +1100

    Add yaml schema configuration for yaml language server

---
+++ nvim/lua/plugins/lsp/servers/yamlls.lua
@@ -0,0 +1,10 @@
+return {
+    settings = {
+        yaml = {
+            schemaStore = {
+                url = "https://www.schemastore.org/api/json/catalog.json",
+                enable = true,
+            },
+        },
+    },
+}
