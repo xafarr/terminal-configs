34351ea3db2322dfb3b702909dd8a343b2fe2b19
Muhammad Zafar <xafarr@gmail.com>
2023-03-20 22:35:04 +1100

    Add config for nvimtree when directory is passed as vim argument

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -10,6 +10,28 @@ return {
             vim.g.loaded_netrw = 1
             vim.g.loaded_netrwPlugin = 1
 
+            local function open_nvim_tree(data)
+                local directory = vim.fn.isdirectory(data.file) == 1
+                if not directory then
+                    return
+                end
+
+                -- create a new, empty buffer
+                vim.cmd.enew()
+
+                -- wipe the directory buffer
+                vim.cmd.bw(data.buf)
+
+                -- change to the directory
+                vim.cmd.cd(data.file)
+
+                -- open the tree
+                require("nvim-tree.api").tree.open()
+            end
+
+            -- NvimTree start up
+            vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
             local icons = require("config.icons")
             local nvim_tree = require("nvim-tree")
             local nvim_tree_config = require("nvim-tree.config")
