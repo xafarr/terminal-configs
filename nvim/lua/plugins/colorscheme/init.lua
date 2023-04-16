0e6085ea7425509ac961ff56aa6dad5b0f87cc53
Muhammad Zafar <xafarr@gmail.com>
2023-04-16 14:42:42 +1000

    Remove unnecessary plugin

--- nvim/lua/plugins/colorscheme/init.lua
+++ nvim/lua/plugins/colorscheme/init.lua
@@ -37,30 +37,4 @@ return {
       })
     end
   },
-  {
-    "projekt0n/github-nvim-theme",
-    config = function()
-      local github = require("github-theme")
-
-      github.setup({
-        colors = {},
-        comment_style = "italic",
-        dark_float = false,
-        dark_sidebar = true,
-        dev = false,
-        function_style = "NONE",
-        hide_end_of_buffer = true,
-        hide_inactive_statusline = true,
-        keyword_style = "italic",
-        msg_area_style = "NONE",
-        overrides = function()
-          return {}
-        end,
-        sidebars = {},
-        theme_style = "light",
-        transparent = false,
-        variable_style = "NONE",
-      })
-    end
-  },
 }
