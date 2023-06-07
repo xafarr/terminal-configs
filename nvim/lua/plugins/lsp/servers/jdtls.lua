4b48be20fbec6b51ed4d9c0d3f4659fce2360c65
Muhammad Zafar <xafarr@gmail.com>
2023-06-07 18:19:21 +1000

    Fix format for jdtls.lua and fix root_dir error

--- nvim/lua/plugins/lsp/servers/jdtls.lua
+++ nvim/lua/plugins/lsp/servers/jdtls.lua
@@ -1,81 +1,83 @@
 local jdtls = require("jdtls")
-local home = os.getenv "HOME"
+local home = os.getenv("HOME")
 local workspace_path = home .. "/.local/share/nvim/jdtls-workspace/"
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = workspace_path .. project_name
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
 extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
 local os_config = "linux"
-if vim.fn.has "mac" == 1 then
-  os_config = "mac"
+if vim.fn.has("mac") == 1 then
+    os_config = "mac"
 end
 
 local config = {
-  cmd = {
-    "java",
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-Xms1g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
-    "-jar",
-    vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-    "-configuration",
-    home .. "/.local/share/nvim/mason/packages/jdtls/config_" .. os_config,
-    "-data",
-    workspace_dir,
-  },
- -- root_dir = require("jdtls.setup").find_root { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" },
-
-  settings = {
-    java = {
-      eclipse = {
-        downloadSources = true,
-      },
-      configuration = {
-        updateBuildConfiguration = "interactive",
-        runtimes = {
-          {
-            name = "JavaSE-11",
-            path = "~/.sdkman/candidates/java/11.0.19-tem",
-          },
-          {
-            name = "JavaSE-17",
-            path = "~/.sdkman/candidates/java/17.0.7-tem",
-          },
-        },
-      },
-      maven = {
-        downloadSources = true,
-      },
-      referencesCodeLens = {
-        enabled = true,
-      },
-      references = {
-        includeDecompiledSources = true,
-      },
-      inlayHints = {
-        parameterNames = {
-          enabled = "all", -- literals, all, none
-        },
-      },
-      format = {
-        enabled = false,
-      },
+    cmd = {
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xms1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+        "-jar",
+        vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+        "-configuration",
+        home .. "/.local/share/nvim/mason/packages/jdtls/config_" .. os_config,
+        "-data",
+        workspace_dir,
     },
-    signatureHelp = { enabled = true },
-    extendedClientCapabilities = extendedClientCapabilities,
-  },
-  init_options = {
-    -- bundles = bundles,
-  },
+    root_dir = function()
+        require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" })
+    end,
+
+    settings = {
+        java = {
+            eclipse = {
+                downloadSources = true,
+            },
+            configuration = {
+                updateBuildConfiguration = "interactive",
+                runtimes = {
+                    {
+                        name = "JavaSE-11",
+                        path = "~/.sdkman/candidates/java/11.0.19-tem",
+                    },
+                    {
+                        name = "JavaSE-17",
+                        path = "~/.sdkman/candidates/java/17.0.7-tem",
+                    },
+                },
+            },
+            maven = {
+                downloadSources = true,
+            },
+            referencesCodeLens = {
+                enabled = true,
+            },
+            references = {
+                includeDecompiledSources = true,
+            },
+            inlayHints = {
+                parameterNames = {
+                    enabled = "all", -- literals, all, none
+                },
+            },
+            format = {
+                enabled = false,
+            },
+        },
+        signatureHelp = { enabled = true },
+        extendedClientCapabilities = extendedClientCapabilities,
+    },
+    init_options = {
+        bundles = {},
+    },
 }
 
 return config
