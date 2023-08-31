4c54611012e4ca3ec9f2950375104c5b33f638cd
Muhammad Zafar <xafarr@gmail.com>
2023-08-31 11:55:47 +1000

    Add more configuration for Windows Powershell and moved into respective directories

--- terminal/windows/powershell/Microsoft.PowerShell_profile.ps1
+++ terminal/windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 # Generally found in $HOME\Documents\PowerShell
 
+Set-Alias vim nvim.exe
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 Invoke-Expression (&starship init powershell)
-
