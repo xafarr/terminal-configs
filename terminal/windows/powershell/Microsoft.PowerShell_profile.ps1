2dc3a283db81559b8d56d9b49d347869f10cd440
Muhammad Zafar <xafarr@gmail.com>
2023-10-11 10:52:43 +1100

    Set which as alias to where.exe in Powershell config

--- terminal/windows/powershell/Microsoft.PowerShell_profile.ps1
+++ terminal/windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 # Generally found in $HOME\Documents\PowerShell
 
 Set-Alias vim nvim.exe
+Set-Alias which where.exe
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 # Import-Module WebAdministration
 Invoke-Expression (&starship init powershell)
