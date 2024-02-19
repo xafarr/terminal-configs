3106242f381939ad512dc541cca0204681b24e54
Muhammad Zafar <xafarr@gmail.com>
2024-02-19 11:32:21 +1100

    Reduce the opacity of the terminal in neovim

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -22,7 +22,7 @@ return {
                 shell = vim.o.shell,
                 float_opts = {
                     border = "curved",
-                    winblend = 0,
+                    winblend = 3,
                     highlights = {
                         border = "Normal",
                         background = "Normal",
