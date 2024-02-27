d104419b76efaabe0ab9d6a4edd388a05f6ea96a
Muhammad Zafar <xafarr@gmail.com>
2024-02-28 00:33:38 +1100

    Add conditional configuration for conform and null-ls for formatting in neovim

--- nvim/lua/plugins/lsp/config/formatter.lua
+++ nvim/lua/plugins/lsp/config/formatter.lua
@@ -1,40 +1,45 @@
 local M = {}
 
 function M.format(bufnr, range)
-  local buf = bufnr or vim.api.nvim_get_current_buf()
-  local ft = vim.bo[buf].filetype
-  local have_nls = #require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0
+  local have_nls, _ = pcall(require, "nvimtools/none-ls.nvim")
 
-  vim.lsp.buf.format({
-    bufnr = buf,
-    async = false,
-    range = range,
-    filter = function(client)
-      if have_nls then
-        return client.name == "null-ls"
-      end
-      return client.name ~= "null-ls"
-    end,
-  })
+  if have_nls then
+    vim.lsp.buf.format({
+      bufnr = bufnr,
+      async = false,
+      range = range,
+      filter = function(client)
+        if have_nls then
+          return client.name == "null-ls"
+        end
+        return client.name ~= "null-ls"
+      end,
+    })
+  else
+    require("conform").format({
+      bufnr = bufnr,
+      async = false,
+      timeout_ms = 500,
+      lsp_fallback = true,
+      range = range,
+    })
+  end
 end
 
 -- Autoformat on save
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-function M.on_attach(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        if not neoconfigs.format_on_save then
-          return
-        end
-        M.format(bufnr)
-      end,
-    })
-  end
+function M.on_attach(_, bufnr)
+  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup,
+    buffer = bufnr,
+    callback = function()
+      if not neoconfigs.format_on_save then
+        return
+      end
+      M.format(bufnr)
+    end,
+  })
 end
 
 return M
