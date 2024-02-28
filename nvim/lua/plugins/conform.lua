f2b09675951fd1941449bf50783cdaa83e8657c7
Muhammad Zafar <xafarr@gmail.com>
2024-02-28 11:05:00 +1100

    Add and configure formatters and linters

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,35 +1,49 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local conform = require("conform")
-            conform.setup({
-                formatters_by_ft = {
-                    -- Conform will run multiple formatters sequentially
-                    --  go = { "goimports", "gofmt" },
-                    -- Use a sub-list to run only the first available formatter
-                    --  javascript = { { "prettierd", "prettier" } },
-                    css = { "prettier" },
-                    go = { "goimports", "gofumpt" },
-                    graphql = { "prettier" },
-                    html = { "prettier" },
-                    java = { "google-java-format" },
-                    javascript = { "prettier" },
-                    javascriptreact = { "prettier" },
-                    json = { "prettier" },
-                    lua = { "stylua" },
-                    markdown = { "markdownlint" },
-                    python = { "isort", "black" },
-                    sql = { "sql-formatter" },
-                    svelte = { "prettier" },
-                    terraform = { "terraform_fmt" },
-                    typescript = { "prettier" },
-                    typescriptreact = { "prettier" },
-                    xml = { "xmlformatter" },
-                    yaml = { "prettier" },
-                },
-            })
-        end,
-    },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local conform = require("conform")
+      conform.setup({
+        formatters_by_ft = {
+          -- Conform will run multiple formatters sequentially
+          --  go = { "goimports", "gofmt" },
+          -- Use a sub-list to run only the first available formatter
+          --  javascript = { { "prettierd", "prettier" } },
+          css = { "prettier" },
+          go = { "goimports", "gofumpt" },
+          graphql = { "prettier" },
+          html = { "prettier" },
+          java = { "google-java-format" },
+          javascript = { "prettier" },
+          javascriptreact = { "prettier" },
+          json = { "prettier" },
+          lua = { "stylua" },
+          markdown = { "markdownlint" },
+          mdx = { "markdownlint" },
+          python = { "isort", "black" },
+          sql = { "sql_formatter" },
+          svelte = { "prettier" },
+          terraform = { "terraform_fmt" },
+          tf = { "terraform_fmt" },
+          typescript = { "prettier" },
+          typescriptreact = { "prettier" },
+          xml = { "xmlformat" },
+          yaml = { "yamlfmt" },
+          bash = { "beautysh" },
+          sh = { "beautysh" },
+          zsh = { "beautysh" },
+          csharp = { "csharpier" },
+          cs = { "csharpier" },
+          kotlin = { "ktlint" },
+          kt = { "ktlint" },
+        },
+        formatters = {
+          markdownlint = {
+            prepend_args = { "--config", neoconfigs.stdConfigPath .. "/markdownlint.json" },
+          },
+        },
+      })
+    end,
+  },
 }
--- nvim/lua/plugins/nvimlint.lua
+++ nvim/lua/plugins/nvimlint.lua
@@ -10,8 +10,19 @@ return {
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
+      markdown = { "markdownlint" },
+      mdx = { "markdownlint" },
       svelte = { "eslint_d" },
       python = { "pylint" },
+      terraform = { "tflint" },
+      tf = { "tflint" },
+      go = { "golangcilint" },
+      bash = { "shellcheck" },
+      sh = { "shellcheck" },
+    }
+
+    lint.linters.markdownlint = {
+      args = { "--config", neoconfigs.stdConfigPath .. "/markdownlint.json" },
     }
   end,
 }
