984f7aaf32d8d32a3ad357efbbde03365508c962
Muhammad Zafar <xafarr@gmail.com>
2024-02-29 19:50:30 +1100

    Attach LSP keymaps when nvim-lint is triggered and setup markdownlint according to custom config

--- nvim/lua/plugins/nvimlint.lua
+++ nvim/lua/plugins/nvimlint.lua
@@ -4,6 +4,10 @@ return {
   events = { "BufWritePost", "BufReadPost", "InsertLeave" },
   config = function()
     local lint = require("lint")
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    -- Attach LSP Keymaps
+    require("plugins.lsp.config.keymaps").on_attach(nil, bufnr)
 
     lint.linters_by_ft = {
       javascript = { "eslint_d" },
@@ -21,8 +25,18 @@ return {
       sh = { "shellcheck" },
     }
 
-    lint.linters.markdownlint = {
-      args = { "--config", neoconfigs.stdConfigPath .. "/markdownlint.json" },
-    }
+    lint.linters.markdownlint = function()
+      local efm = "%f:%l:%c %m,%f:%l %m"
+      return {
+        cmd = neoutils.IS_WINDOWS() and "markdownlint.cmd" or "markdownlint",
+        ignore_exitcode = true,
+        stream = "stderr",
+        parser = require("lint.parser").from_errorformat(efm, {
+          source = "markdownlint",
+          severity = vim.diagnostic.severity.WARN,
+        }),
+        args = { "--config", neoconfigs.stdConfigPath .. "/markdownlint.json" },
+      }
+    end
   end,
 }
