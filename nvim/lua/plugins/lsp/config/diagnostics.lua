6739cc2ba283ef35403613f813fb518ab58d6e6b
Muhammad Zafar <xafarr@gmail.com>
2024-02-29 20:07:28 +1100

    Add noice.nvim plugin and set hover and signatureHelp to be handled by it as well

--- nvim/lua/plugins/lsp/config/diagnostics.lua
+++ nvim/lua/plugins/lsp/config/diagnostics.lua
@@ -43,9 +43,9 @@ function M.setup_diagnostics()
 
   vim.diagnostic.config(config.diagnostics)
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, config.float)
+  -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, config.float)
 
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, config.float)
+  -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, config.float)
 end
 
 return M
