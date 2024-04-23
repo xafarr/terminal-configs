5cdad16bcc4cf711e48ddd4f484a053087c5b08f
Muhammad Zafar <xafarr@gmail.com>
2024-04-23 14:16:47 +1000

    Fix neorg after upgrading it to version 8 which has breaking changes

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,26 +1,30 @@
 return {
-  "nvim-neorg/neorg",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  build = ":Neorg sync-parsers",
-  -- tag = "*",
-  lazy = true, -- enable lazy load
-  ft = "norg", -- lazy load on file type
-  cmd = "Neorg", -- lazy load on command
-  config = function()
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/Documents/Nextcloud/Neorg/notes",
-              tuts = "~/Documents/Nextcloud/Neorg/tutorials",
-              howtos = "~/Documents/Nextcloud/Neorg/howtos",
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false,
+    version = "*",
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Documents/Nextcloud/Neorg/notes",
+                tuts = "~/Documents/Nextcloud/Neorg/tutorials",
+                howtos = "~/Documents/Nextcloud/Neorg/howtos",
+              },
             },
           },
         },
-      },
-    })
-  end,
+      })
+    end,
+  },
 }
