738a7e81dadb6c562ea087ad916060e6070dad6b
Muhammad Zafar <xafarr@gmail.com>
2024-05-10 15:58:38 +1000

    Install kitty last in auto setup

--- setup.sh
+++ setup.sh
@@ -192,29 +192,6 @@ fi
 if ! [ -f "$BREW_BIN/bat" ]; then
     $BREW install bat || true
 fi
-if ! [ -f "$BREW_BIN/kitty" ] && [ -n "${SETUP_ON_MACOS-}" ]; then
-    $BREW install --cask kitty || true
-else
-    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-    # Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
-    # your system-wide PATH)
-    local_bin="${HOME}/.local/bin"
-    local_share_app="${HOME}/.local/share/applications"
-    if [[ ! -d "$local_bin" ]]; then
-        mkdir -p "$local_bin"
-    fi
-    if [[ ! -d "$local_share_app" ]]; then
-        mkdir -p "$local_share_app"
-    fi
-    ln -sf "$HOME/.local/kitty.app/bin/kitty" "$HOME/.local/kitty.app/bin/kitten" "$local_bin"
-    # Place the kitty.desktop file somewhere it can be found by the OS
-    cp "$HOME/.local/kitty.app/share/applications/kitty.desktop" "$HOME/.local/share/applications/"
-    # If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
-    cp "$HOME/.local/kitty.app/share/applications/kitty-open.desktop" "$HOME/.local/share/applications/"
-    # Update the paths to the kitty and its icon in the kitty desktop file(s)
-    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-    sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-fi
 if [[ $($BREW list | grep -iwc bzip2) -eq 0 ]]; then
     $BREW install bzip2 || true
 fi
@@ -398,3 +375,29 @@ echo "Installing NodeJS"
 ($ASDF plugin-add nodejs && $ASDF install nodejs latest:20 && $ASDF global nodejs latest:20) || true
 echo "Installing Golang"
 ($ASDF plugin-add golang && $ASDF install golang latest && $ASDF global golang latest) || true
+
+# Install Kitty terminal emulator
+echo "Installing Kitty terminal emulator..."
+if ! [ -f "$BREW_BIN/kitty" ] && [ -n "${SETUP_ON_MACOS-}" ]; then
+    $BREW install --cask kitty || true
+else
+    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+    # Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
+    # your system-wide PATH)
+    local_bin="${HOME}/.local/bin"
+    local_share_app="${HOME}/.local/share/applications"
+    if [[ ! -d "$local_bin" ]]; then
+        mkdir -p "$local_bin"
+    fi
+    if [[ ! -d "$local_share_app" ]]; then
+        mkdir -p "$local_share_app"
+    fi
+    ln -sf "$HOME/.local/kitty.app/bin/kitty" "$HOME/.local/kitty.app/bin/kitten" "$local_bin"
+    # Place the kitty.desktop file somewhere it can be found by the OS
+    cp "$HOME/.local/kitty.app/share/applications/kitty.desktop" "$HOME/.local/share/applications/"
+    # If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+    cp "$HOME/.local/kitty.app/share/applications/kitty-open.desktop" "$HOME/.local/share/applications/"
+    # Update the paths to the kitty and its icon in the kitty desktop file(s)
+    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+    sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+fi
