de6fcfab124d813110f00c7efa81e683d2397dc3
Muhammad Zafar <xafarr@gmail.com>
2024-05-10 15:26:09 +1000

    Fix the issue with regex in sed command in auto script

--- setup.sh
+++ setup.sh
@@ -212,8 +212,8 @@ else
     # If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
     cp "$HOME/.local/kitty.app/share/applications/kitty-open.desktop" "$HOME/.local/share/applications/"
     # Update the paths to the kitty and its icon in the kitty desktop file(s)
-    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" "$HOME/.local/share/applications/kitty*.desktop"
-    sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" "$HOME/.local/share/applications/kitty*.desktop"
+    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+    sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 fi
 if [[ $($BREW list | grep -iwc bzip2) -eq 0 ]]; then
     $BREW install bzip2 || true
