878c16073c2799116467c60f80eb742084944d9d
Muhammad Zafar <xafarr@gmail.com>
2024-05-27 13:14:32 +1000

    Updated the build script for telescope-fzf-native according to the documentation

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,6 +2,13 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        cond = function()
+          return vim.fn.executable("cmake") == 1
+        end,
+      },
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
       "nvim-telescope/telescope-file-browser.nvim",
       "nvim-telescope/telescope-project.nvim",
