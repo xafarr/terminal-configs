3f3f4f81693d6f61f52d190f7617c8985439e73b
Muhammad Zafar <xafarr@gmail.com>
2024-12-16 12:36:25 +1100

    Change MarkdownPreview plugin config in neovim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -30,9 +30,9 @@ return {
   },
   {
     "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    config = function()
-      vim.g.mkdp_filetypes = { "markdown" }
+    build = function()
       vim.fn["mkdp#util#install"]()
     end,
   },
