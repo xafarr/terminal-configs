d4c3dca99e8b9c6ee2df6c2ed97c0971de8537c7
Muhammad Zafar <xafarr@gmail.com>
2025-04-17 01:56:25 +1000

    Remove eslint_d as typescript linter

--- nvim/lua/plugins/nvimlint.lua
+++ nvim/lua/plugins/nvimlint.lua
@@ -4,13 +4,10 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local lint = require("lint")
-    local bufnr = vim.api.nvim_get_current_buf()
 
     lint.linters_by_ft = {
       javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
       markdown = { "markdownlint-cli2" },
       mdx = { "markdownlint-cli2" },
       go = { "golangcilint" },
