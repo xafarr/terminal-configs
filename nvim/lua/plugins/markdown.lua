22c447c7ae7d6cc040439c838aa6d45e513d73a8
Muhammad Zafar <xafarr@gmail.com>
2025-04-16 23:09:36 +1000

    Use yarn to build markdown-preview.nvim instead of npm

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -49,7 +49,7 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
+    build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
