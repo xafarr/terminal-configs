8eb140a64f441e568e60cb6444806d62f03aad75
Muhammad Zafar <xafarr@gmail.com>
2025-05-31 23:44:36 +1000

    Modified LSP config to conform with Mason 2.0 update in nvim

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -2,8 +2,19 @@ return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    {"mason-org/mason.nvim", version = "1.11.0"},
-    {"mason-org/mason-lspconfig.nvim", version = "1.32.0"},
+    {
+      "mason-org/mason.nvim",
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      },
+    },
+    "mason-org/mason-lspconfig.nvim",
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
     {
@@ -17,9 +28,6 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
-    -- import lspconfig plugin
-    local lspconfig = require("lspconfig")
-    local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
     local mason_tool_installer = require("mason-tool-installer")
 
@@ -40,50 +48,34 @@ return {
       has_cmp and cmp_nvim_lsp.default_capabilities() or {}
     )
 
+    vim.lsp.config("*", {
+      on_attach = neoutils.on_attach(function(client, bufnr)
+        formatter.on_attach(client, bufnr)
+        if client:supports_method("textDocument/semanticTokens") then
+          client.server_capabilities.semanticTokensProvider = nil
+        end
+      end),
+      capabilities = capabilities,
+      flags = {
+        debounce_text_changes = 150,
+      },
+    })
+
+    for _, server in ipairs(neoconfigs.language_servers) do
+      local extend, user_config = pcall(require, "plugins.lsp.servers." .. server)
+      if extend then
+        vim.lsp.config(server, user_config.setup_and_get_config())
+      end
+    end
+
     -- Setup neodev lua configuration
     require("neodev").setup({
       library = { plugins = { "nvim-dap-ui" }, types = true },
     })
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-
     mason_lspconfig.setup({
       ensure_installed = neoconfigs.language_servers,
-      automatic_installation = false,
-      handlers = {
-        function(server)
-          local opts = {
-            -- Attach formatter to LSP
-            on_attach = neoutils.on_attach(function(client, bufnr)
-              formatter.on_attach(client, bufnr)
-              if client:supports_method("textDocument/semanticTokens") then
-                client.server_capabilities.semanticTokensProvider = nil
-              end
-            end),
-            capabilities = capabilities,
-            flags = {
-              debounce_text_changes = 150,
-            },
-          }
-
-          local extend, user_config = pcall(require, "plugins.lsp.servers." .. server)
-
-          if extend then
-            opts = vim.tbl_deep_extend("force", opts, user_config.setup_and_get_config())
-          end
-          if server ~= "jdtls" then
-            lspconfig[server].setup(opts)
-          end
-        end,
-      },
+      automatic_enable = { exclude = { "jdtls" } },
     })
 
     mason_tool_installer.setup({
